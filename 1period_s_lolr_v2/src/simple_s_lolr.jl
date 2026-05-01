a174d789ca43639d8f8a517b1e6049c8d9936523
Erfan Ahar <ahar.erfan@gmail.com>
2026-05-01 15:25:35 -0500

    renaming

--- 1period_s_lolr_v2/src/simple_s_lolr.jl
+++
@@ -1,4 +0,0 @@
-include("structs_s_lolr.jl")
-include("utils_s_lolr.jl")
-include("solver_s_lolr.jl")
-include("simulation_s_lolr.jl")
---
+++ 1period_s_lolr_v2/src/simple_s_lolr_v2.jl
@@ -0,0 +1,4 @@
+include("structs_s_lolr_v2.jl")
+include("utils_s_lolr_v2.jl")
+include("solver_s_lolr_v2.jl")
+include("simulation_s_lolr_v2.jl")
